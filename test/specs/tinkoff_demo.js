30542816bc8967e58bc9dabe801e7d54fe546756
dorrin <trukhin.work@gmail.com>
2017-10-20 07:36:15 +0300

    Update tinkoff_demo.js

--- test/specs/tinkoff_demo.js
+++ test/specs/tinkoff_demo.js
@@ -10,9 +10,8 @@ const ZhkuMoskvaOplataPage1 = new ZhkuMoskvaOplataPage(browser);
 
 suite('/TINKOFF DEMO/', () => {
 
-let url = "http://tinkoff.ru";
-let testString = "ЖКУ-Москва";
-let testTitle;
+const url = "http://tinkoff.ru";
+const testString = "ЖКУ-Москва";
 
    suiteSetup ('Preconditions (suite setup)', () => {
    });
@@ -49,7 +48,8 @@ let testTitle;
       expect(ZhkuMoskvaOplataPage1.pole_kod_err.getText()).to.equal("Поле обязательное");
       expect(ZhkuMoskvaOplataPage1.pole_date_err.getText()).to.equal("Поле обязательное");
       expect(ZhkuMoskvaOplataPage1.pole_summ_err.getText()).to.equal("Поле обязательное");
-
+      
+//Значения для полей выбраны как заведомо обязанные вызывать ошибку
       ZhkuMoskvaOplataPage1.pole_kod.setValue(0);
       ZhkuMoskvaOplataPage1.pole_date.setValue(0);
       ZhkuMoskvaOplataPage1.pole_summ.setValue(9);
@@ -57,7 +57,6 @@ let testTitle;
       expect(ZhkuMoskvaOplataPage1.pole_kod_err.getText()).to.equal("Поле неправильно заполнено");
       expect(ZhkuMoskvaOplataPage1.pole_date_err.getText()).to.equal("Поле заполнено некорректно");
       expect(ZhkuMoskvaOplataPage1.pole_summ_err.getText()).to.equal("Минимальная сумма перевода - 10 ₽");
-
 
       ZhkuMoskvaOplataPage1.pole_kod.setValue(111);
       ZhkuMoskvaOplataPage1.pole_date.setValue(999);
@@ -72,7 +71,6 @@ let testTitle;
       expect(PayPage1.pay_text_list(1).getText()).to.include(testString);
       PayPage1.pay_text_list(1).click();
 
-      
       ZhkuMoskvaPage1.oplata_link.waitForVisible();
       expect(browser.getUrl()).to.equal(testTitle);
 
@@ -84,7 +82,7 @@ let testTitle;
       KommPayPage1.region_item('г. Санкт-Петербург').click();
 
 //Цикл для скроллинга страницы, для отображения всех элементов.
-
+//Pause - костыль, можно без него, например подсчитывая количество подгруженных в DOM дочерних элементов списка иконок
       let y = 0;
       while (y != KommPayPage1.footer.getLocation("y")) {
          y = KommPayPage1.footer.getLocation("y");
